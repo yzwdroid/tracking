188fe79caedb988860f471ec67c7ac4e8832215d
Zongwei Yang <yzwdroid@gmail.com>
2022-02-27 00:20:00 -0500

    init commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+import shutil
+from os import listdir
+from os.path import isfile, join
+from pathlib import Path
+
+baseURL = "/Users/zyang/src/project/"
+
+while True:
+    code = input("please enter the tracking code: ")
+    folder = join(baseURL, code)
+    donePath = join(folder, "done")
+    Path(donePath).mkdir(parents=True, exist_ok=True)
+    allFiles = [f for f in listdir(folder) if isfile(join(folder, f))]
+
+    if allFiles:
+        file = join(baseURL, code, allFiles[0])
+        desc = join(baseURL, code, "done", allFiles[0])
+        print(file)
+        print(desc)
+    #os.startfile(file, "print")
+    else:
+        print("no files to print")
